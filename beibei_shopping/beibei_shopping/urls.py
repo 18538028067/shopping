894c7af6c946f2966f6bb2d54055d9fc12dd29d1
18538028067 <44199836+18538028067@users.noreply.github.com>
2018-10-22 09:38:47 +0800

    Add files via upload

--- beibei_shopping/beibei_shopping/urls.py
+++ beibei_shopping/beibei_shopping/urls.py
@@ -1,29 +1,13 @@
-"""beibei_shopping URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.conf.urls import url
-from django.conf.urls import include
 
+from . import views
+
+app_name = "users"
 
 urlpatterns = [
-    url(r'admin/', admin.site.urls),
-    url(r'users/', include("users.urls")),
-    url(r'^$',include("commons.urls")),
-    url(r'^commons/',include("commons.urls")),
-    url(r'^store/',include("store.urls")),
-    url(r'^goods/',include("goods.urls")),
+    url(r"^login/$",views.user_login,name="user_login"),
+    url(r"^register/$",views.user_register,name="user_register"),
+    url(r"^logout/$",views.user_logout,name="user_logout"),
+    url(r"^userinfo/$",views.user_info,name="user_info"),
 
 ]
---
+++ beibei_shopping/beibei_shopping/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+
+from . import models
+
+
+
+def user_login(request):
+    '''用户登录'''
+    if request.method == "GET":
+        try:
+            next_url = request.GET["next"]
+        except:
+            next_url = "/"
+
+        return render(request, "users/login.html", {"next_url":next_url})
+    elif request.method == "POST":
+        username = request.POST["username"].strip()
+        password = request.POST["password"].strip()
+
+        next_url = request.POST.get("next","/")
+
+        # 验证码
+        # code = request.POST["code"].strip()
+
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            if user.is_active:
+                # 将验证通过的用户信息保存在request
+                login(request,user)
+                return redirect(next_url)
+            else:
+                return render(request, "users/login.html", {"msg": "您的账号已经被锁定，请联系管理员"})
+        else:
+            return render(request, "users/login.html", {"msg": "账号或者密码有误"})
+
+
+def user_register(request):
+    '''用户注册'''
+    if request.method == "GET":
+        return render(request,"users/register.html",{"msg":"请认真填写如下选项"})
+    elif request.method == "POST":
+        # 接受参数
+
+        username = request.POST.get("username").strip()
+        password = request.POST.get("password").strip()
+        confirmpwd = request.POST.get("confirmpwd").strip()
+        nickname = request.POST.get("nickname").strip()
+
+        # 数据校验
+        if len(username) < 1:
+            return render(request,"users/register.html",{"msg":"用户名不能为空！！"})
+
+        if len(password) < 6:
+            return render(request,"users/register.html",{"msg":"密码长度不能小于6位！！"})
+
+        if password != confirmpwd:
+            return render(request,"users/register.html",{"msg":"两次密码不一致！！"})
+
+        # 用户名是否重复
+        try:
+            User.objects.get(username=username)
+            return render(request,"users/register.html",{"msg":"用户名重复！"})
+        except:
+
+            try:
+                # 判断昵称是否重复
+                models.UserInfo.objects.get(nickname=nickname)
+                return render(request,"users/register.html",{"msg":"昵称重复！"})
+            except:
+                # 保存用户信息
+                user = User.objects.create_user(username=username,password=password)
+                user_info = models.UserInfo(nickname=nickname,user=user)
+                user.save()
+                user_info.save()
+                return render(request,"users/login.html",{"msg":"用户注册成功"})
+
+
+
+@login_required
+def user_logout(request):
+    '''注销用户'''
+    logout(request)
+    return render(request, "users/login.html", {"msg": "退出成功！"})
+
+
+@login_required
+def user_info(request):
+
+    return render(request,"users/userinfo.html",{})
+
